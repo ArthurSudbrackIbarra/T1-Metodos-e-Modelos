c3d9c3acbe96dcec1b972c461f7ec38dd7df68c0
khaelk <klkable13@gmail.com>
2023-09-10 03:25:20 -0300

    getall_pdfs feature

---
+++ app/test/features/steps/getall_pdfs.py
@@ -0,0 +1,29 @@
+from fastapi.testclient import TestClient
+from behave import given, when, then
+from app.main import app
+from app.api.models.pdf import PDF
+
+# Create a TestClient instance to interact with the FastAPI application
+test_app = TestClient(app=app)
+
+# Variable to store the response
+response = None
+
+@given("I am using the application")
+def step_given_application_running(context):
+    pass
+
+@when(r'I send a GET request to "/pdfs"')
+def step_when_send_get_request(context):
+    global response
+    response = test_app.get("/pdfs")
+    context.response = response
+
+@then("the GET response status code should be 200 OK")
+def step_then_check_status_code(context):
+    assert context.response.status_code == 200
+
+@then("the response should contain at least one PDF")
+def step_then_check_response_contains_pdf(context):
+    response_data = context.response.json()
+    assert len(response_data) > 0
